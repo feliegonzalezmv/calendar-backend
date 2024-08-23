9bbd3f2e4b279b9fe79967c7a7e17e93243d79f3
feliegonzalezmv <53414652+feliegonzalezmv@users.noreply.github.com>
2024-08-23 15:26:42 -0500

    Inicial config ts and express

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world");
+import express from "express";
+
+const app = express();
+
+//Routes
+
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+app.listen(4000, () => {
+  console.log("server running on PORT:>> ", 4000);
+});
