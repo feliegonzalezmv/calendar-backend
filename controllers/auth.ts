ecbc24559c52dc0e08ff2f413b9565e682b63ee6
feliegonzalezmv <53414652+feliegonzalezmv@users.noreply.github.com>
2024-08-23 19:39:49 -0500

    Create user collection and save user

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,15 +1,37 @@
 import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
 
-export const createUser = (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
+import User from "../models/user";
 
-  res.status(201).json({
-    ok: true,
-    msg: "register",
-    name,
-    email,
-    password,
-  });
+export const createUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res
+        .status(400)
+        .json({ ok: false, msg: "User already exist with this email" });
+    }
+
+    user = new User(req.body);
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
+    await user.save();
+
+    res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Please talk with the administrator",
+    });
+  }
 };
 
 export const loginUser = (req: Request, res: Response) => {
---
+++ models/user.ts
@@ -0,0 +1,9 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+  name: { type: String, require: true },
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+});
+
+export default model("User", UserSchema);
